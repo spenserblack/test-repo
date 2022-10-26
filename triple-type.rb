3f766c6d121ea349489a608e5b7c5125911b4e36
Spenser Black <spenserblack01@gmail.com>
2022-10-26 16:11:54 -0400

    Filenames, shebangs, and modelines

---
+++ triple-type.rb
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# vim: set ft=typescript:
+# The file looks like Ruby, it runs like Python, but GitHub thinks it's TypeScript.
+import sys
+
+def main():
+    print('Hello, world!')
+    return 0
+
+if __name__ == '__main__':
+    sys.exit(main())
